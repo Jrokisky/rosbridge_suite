ce6a38520ba571fe7342e6ee02d486a516e00f53
Justin Rokisky <jrokisky831@gmail.com>
2018-06-27 14:49:18 -0400

    Improved rosbridge_push client's ability to handle the server going offline

--- rosbridge_push/src/rosbridge_push/rosbridge_push.py
+++ rosbridge_push/src/rosbridge_push/rosbridge_push.py
@@ -16,34 +16,41 @@ class RosbridgePushClient():
             "unregister_timeout": 10,
             "bson_only_mode": False
         }
+        self.ws = None
         self.protocol = RosbridgeProtocol("main", parameters=parameters)
         self.protocol.outgoing = self.send
-        self.connected = False
 
     def connect(self):
         try:
             self.ws = websocket.create_connection(self.url)
-            self.connected = True
             rospy.loginfo("Connected to: %s", self.url)
         except Exception as exc:
             rospy.logerr("Unable to connect to server.  Reason: %s", str(exc))
-            self.connected = False
 
     def isConnected(self):
-        return self.connected
+        if self.ws is None:
+            return False
+        else:
+            return self.ws.connected
 
     def send(self, message):
-        rospy.loginfo("Sending message!")
-        self.ws.send(message)
+        if self.ws.connected:
+            rospy.loginfo("Sending message!")
+            self.ws.send(message)
 
     def recv(self):
-        result = self.ws.recv()
-        rospy.loginfo(result);
-        self.protocol.incoming(result);
+        try:
+            result = self.ws.recv()
+            rospy.loginfo(result);
+            self.protocol.incoming(result);
+        except websocket.WebSocketConnectionClosedException as exc:
+            rospy.logerror("Server is not responding. Error: %s", str(exc))
+
 
     def close(self):
+        # TODO: should we rebuild the Rosbridge Protocol after a disconnect?
         rospy.loginfo("Shutting down rosbridge_push")
         if self.isConnected():
             rospy.loginfo("Closed websocket to: %s", self.url)
             self.ws.close()
-            self.connected = False
+            self.ws = None
